f9ac2565789da3ff15ca760df8f85655aa6cec11
gopinath <gopinaths2301@gmail.com>
2023-09-19 23:54:11 +0530

    resourcecard

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,18 @@
+import ResourceCard from '@/components/ResourceCard'
 import SearchForm from '@/components/SearchForm'
 import Filter from '@/components/ui/Filter'
+import { getResources } from '@/sanity/actions'
 import React from 'react'
+export const revalidate=900;
 
-const Page = () => {
+const Page = async() => {
+  const Resources = await getResources({
+    query: '',
+    category:'',
+    page:'1'
+  })
+  console.log(Resources);
+  
   return (
     <main className='flex-center paddings mx-auto w-full max-w-screen-2xl flex-col'>
 <section className='nav-padding w-full'>
@@ -10,6 +20,14 @@ const Page = () => {
     <h1 className='sm:heading1 heading2 mb-6 text-center text-white'>Javascript Mastery Resources</h1>
   </div>
   <SearchForm/>
+  <section className="flex-center mt-6 w-full flex-col sm:mt-20">
+   Header
+   <div className='mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start'>
+  {Resources?.length>0 ? (Resources.map((resource:any)=>(
+    <ResourceCard key={resource._id} title={resource.title} image={resource.image} id={resource._id} downloadNumber={resource.views} slug={resource.slug}/>
+  ))):(<p className='body-regular text-white-400'>No resources found</p>)}
+   </div>
+  </section>
 </section>
 <Filter/>
     </main>
--- sanity/actions.ts
+++ sanity/actions.ts
@@ -1,6 +1,7 @@
 import {groq} from 'next-sanity'
 import { buildQuery } from './utils';
 import { readClient } from './lib/client';
+import { readFile } from 'fs/promises';
 
 interface GetResourcesParams {
     query:string;
@@ -11,9 +12,25 @@ export const getResources = async (params: GetResourcesParams)=>{
     const {query, category,page}= params;
     try {
         const resources = await readClient.fetch(
-            groq
+            groq`${buildQuery({
+                type:'resource',
+                query,
+                category,
+                page:parseInt(page)
+            })}{
+                title,
+                _id,
+                downloadLink,
+                "image":poster.asset->url,
+                views,
+                slug,
+                category
+            }`
+           
         )
+        return resources;
     } catch (error) {
+        console.log(error);
         
     }
 }
